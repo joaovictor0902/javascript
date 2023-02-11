90dfcd48777768ea6079bb86f7ab06347feec362
João Victor <111450785+joaovictor0902@users.noreply.github.com>
2023-02-11 16:16:43 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`Velocidade do seu carro é de: ${vel}km/h`) //sempre use o " `` " 
+if(vel > 60){   //condição simples!
+    console.log(`Você ultrapassou a velociade permitida. MULTADO!`)
+}
+console.log(`Sempre dirija usando cinto de segurança!`)
