57eb9fe81d3357b512a36e7e144636b982d27d27
João Victor <111450785+joaovictor0902@users.noreply.github.com>
2023-02-18 20:12:09 -0300

    Update ex013.js

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -9,5 +9,30 @@ var DiaSem = agora.getDay ()
     5 - Sexta
     6 -  Sábado
 
- */  */
- console.log(`Hoje é ${agora}`)
+ */
+ console.log(DiaSem)
+switch(diaSem){
+   case 0:
+      console.log('Domingo')
+      break
+   case 1:
+      console.log('Segunda')
+      break
+   case 2:
+      console.log('Terça')
+      break
+   case 3:
+      console.log('Quarta')
+      break
+   case 4:
+      console.log('Quinta')
+      break
+   case 5:
+      console.log('Sexta')
+      break
+   case 6:
+      console.log('Sábado')
+      break
+   default:
+      break
+}
